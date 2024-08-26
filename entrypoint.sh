1a9fb9898fa66f9e047843d4ba14c712abd7a752
D-setia <dsetia@cs.iitr.ac.in>
2024-08-26 19:21:35 +0530

    feat: Add login support

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ aws eks update-kubeconfig --name tensorkube --region us-east-1
 ls -l
 cd $8 
 
-if [-n "$2"]; then
+export TENSORKUBE_TOKEN=$9
+export TENSORKUBE_SESSION_ID=${10}
+
+if [ -n "$2" ]; then
     if [ -n "$7" ]; then
         tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
     else
