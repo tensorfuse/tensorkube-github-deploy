e77e69c9f5cd65193a78b77e180292a8cb05bf89
D-setia <dsetia@cs.iitr.ac.in>
2024-08-28 18:43:24 +0530

    fix: Use updated tensorkube package and deploy with github-actions flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,14 @@ export TENSORKUBE_SESSION_ID=${10}
 
 if [ -n "$2" ]; then
     if [ -n "$7" ]; then
-        tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
+        tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7 --github-actions
     else
-        tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6
+        tensorkube deploy --gpus $1 --gpu-type $2 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --github-actions
     fi
 else
     if [ -n "$7" ]; then
-        tensorkube deploy --gpus $1 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7
+        tensorkube deploy --gpus $1 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --env $7 --github-actions
     else
-        tensorkube deploy --gpus $1 --cpu $3 --memory $4 --min-scale $5 --max-scale $6
+        tensorkube deploy --gpus $1 --cpu $3 --memory $4 --min-scale $5 --max-scale $6 --github-actions
     fi
 fi
